bdc3d9f6c6c12150d4dff9defd19ac2dd7c81689
Svetlana Klimash <83081643+brendiskok1986@users.noreply.github.com>
2021-10-26 14:27:54 +0300

    Add files via upload

---
+++ Workspace/introduction-to-java-epam/src/by/klimash/part01/main/Fourth.java
@@ -0,0 +1,19 @@
+package by.klimash.part01.main;
+
+public class Fourth {
+
+	public static void main(String[] args) {
+		// TODO Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях). 
+		// Поменять местами дробную и целую части числа и вывести полученное значение числа.
+		
+		double a = 555.222;
+		double b = a * 1000;
+		double c = b % 1000;
+		double d = (b - c) / 1000;
+		double e = c + d / 1000;
+		
+		System.out.println("Начальное число = " + a);
+		System.out.println("Конечное число = " + e);
+	}
+
+}
---
+++ Workspace/introduction-to-java-epam/src/by/klimash/part01/main/Second.java
@@ -0,0 +1,10 @@
+package by.klimash.part01.main;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO  Вычислить значение выражения по формуле (все переменные принимают действительные значения)
+		
+	}
+
+}
