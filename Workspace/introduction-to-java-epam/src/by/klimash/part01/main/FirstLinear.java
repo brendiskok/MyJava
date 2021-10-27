4f58e202c79da848d023ac885b50d91421c8b20d
Svetlana Klimash <sveta-july@yandex.ru>
2021-10-27 14:27:16 +0300

    first tasks have been renamed

---
+++ Workspace/introduction-to-java-epam/src/by/klimash/part01/main/FirstLinear.java
@@ -0,0 +1,17 @@
+package by.klimash.part01.main;
+
+public class FirstLinear {
+
+	public static void main(String[] args) {
+		//TODO Найдите значение функции: z = ((a – 3 ) * b / 2) + c
+
+		double a = 4.5;
+		double b = 2.0;
+		double c = 3.0;
+		
+		double z = ((a - 3) * b / 2) + c;
+		
+		System.out.println("((" + a + " - 3.0 ) * " + b + " / 2.0) + " + c + " = " + z);
+	}
+
+}
