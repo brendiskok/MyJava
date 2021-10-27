717e0cd6daf34644b550df45ebf170e38fb7bd28
Svetlana Klimash <sveta-july@yandex.ru>
2021-10-27 14:53:58 +0300

    add SecondLinear

--- Workspace/introduction-to-java-epam/src/by/klimash/part01/main/SecondLinear.java
+++ Workspace/introduction-to-java-epam/src/by/klimash/part01/main/SecondLinear.java
@@ -3,8 +3,19 @@ package by.klimash.part01.main;
 public class SecondLinear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO  Вычислить значение выражения по формуле (все переменные принимают действительные значения):
+		//(b + sqrt (b2 + 4 ac)) / (2a) - a3c +b(-2)
+		
+		double a = 2.0;
+		double b = 4.0;
+		double c = 3.0;
+		double d = Math.pow(b, 2) + 4 * a * c;
+		
+				
+		double result = (b + Math.sqrt(d)) / ( 2 * a ) - Math.pow(a, 3) * c + Math.pow(b, -2); 
+		
+		System.out.println(result);
+		
 	}
 
 }
