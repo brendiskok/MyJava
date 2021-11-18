57e3a0a78d7ca8be8b488391fd0f35af5e3eb7fc
Svetlana Klimash Pulikhov str <sveta-july@yandex.ru>
2021-11-18 09:59:45 +0300

    changes in syerra beyts practice, chapter 1

---
+++ Workspace/syerra-beyts-javastarter/src/by/sveta/javastarter/main/JustPractice.java
@@ -0,0 +1,21 @@
+package by.sveta.javastarter.main;
+
+import java.util.Scanner;
+
+
+public class JustPractice {
+
+	public static void main(String[] args) {
+		// practice from different sources
+		
+		System.out.println("Введи ФИО");
+		Scanner x = new Scanner (System.in);
+		String y = x.nextLine();
+		
+		System.out.println(y);
+		
+		
+
+	}
+
+}
